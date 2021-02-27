4c67f963b5ae850848d54158b3aaab5c42c5d52e
Ibrahim Abdu <Ibby@Ibrahims-MacBook-Air.local>
2021-02-27 14:19:58 +0000

    mapping single verse

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import quranData from "./qurandata.json"
+     
+      
+      {/* <p>{verses.index}</p>
+      <p>{verses.name}</p>
+      <p>{verses.verse.verse_1}</p>
+      for (i=0; i<verses.verse.length; i++)
+      {
+        <p>verses.verse[i]</p>;
+      }
+      {/* <p>{Object.values(verses.verse)}</p> */}
+      {/* <p>{verses.verse.verse_2}</p>
+      <p>{verses.verse.verse_3}</p>
+      <p>{verses.verse.verse_4}</p>
+      <p>{verses.verse.verse_5}</p>
+      <p>{verses.verse.verse_6}</p>
+      <p>{verses.verse.verse_7}</p> */}
+const quranDataObject = Object.values(quranData.verse)
+const displayedAyat = quranDataObject.map((x) => {
+  return <p>{x}</p>
+})
 function App() {
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>{displayedAyat}</div>
   );
 }
 
